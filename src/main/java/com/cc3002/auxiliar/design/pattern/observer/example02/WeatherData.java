234a5c51df3b9467879fd5f402fcfe2942fe8cf9
Juraj Kubelka <juraj.kubelka@icloud.com>
2016-10-27 23:14:34 -0300

    removes TODO comment

--- src/main/java/com/cc3002/auxiliar/design/pattern/observer/example02/WeatherData.java
+++ src/main/java/com/cc3002/auxiliar/design/pattern/observer/example02/WeatherData.java
@@ -6,31 +6,37 @@ public class WeatherData implements IWeatherDataSetter, IWeatherDataGetter {
   private double temperature;
   private double pressure;
 
+  @Override
   public void measurementsChanged() {
-    // TODO Auto-generated method stub
-
+    /* we should do something here */
   }
 
+  @Override
   public void setTemperature(final double value) {
     temperature = value;
   }
 
+  @Override
   public void setHumidity(final double value) {
     humidity = value;
   }
 
+  @Override
   public void setPressure(final double value) {
     pressure = value;
   }
 
+  @Override
   public double getHumidity() {
     return humidity;
   }
 
+  @Override
   public double getPressure() {
     return pressure;
   }
 
+  @Override
   public double getTemperature() {
     return temperature;
   }
