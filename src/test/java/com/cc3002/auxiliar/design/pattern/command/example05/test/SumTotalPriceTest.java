66a9b94ded21efd3f05291eedb2326fd9451d798
Juraj Kubelka <juraj.kubelka@gmail.com>
2016-11-03 16:21:54 -0300

    refactors tests

--- src/test/java/com/cc3002/auxiliar/design/pattern/command/example05/test/SumTotalPriceTest.java
+++ src/test/java/com/cc3002/auxiliar/design/pattern/command/example05/test/SumTotalPriceTest.java
@@ -1,53 +1,12 @@
 package com.cc3002.auxiliar.design.pattern.command.example05.test;
 
-import static org.junit.Assert.assertEquals;
-
 import com.cc3002.auxiliar.design.pattern.command.example05.AbstractSumPrice;
-import com.cc3002.auxiliar.design.pattern.command.example05.IMenu;
-import com.cc3002.auxiliar.design.pattern.command.example05.IMenuItem;
-import com.cc3002.auxiliar.design.pattern.command.example05.Menu;
-import com.cc3002.auxiliar.design.pattern.command.example05.MenuItem;
 import com.cc3002.auxiliar.design.pattern.command.example05.SumTotalPrice;
 
-import org.junit.Before;
-import org.junit.Test;
+public class SumTotalPriceTest extends AbstractSumPriceTest {
 
-public class SumTotalPriceTest {
-
-  private static final double DOUBLE_PRECISION = 0.000001d;
-  private AbstractSumPrice command;
-  private IMenu menu;
-  private IMenuItem itemOne;
-  private MenuItem itemTwo;
-
-  /**
-   * Initialize test cases.
-   */
-  @Before
-  public void setUp() {
-    command = new SumTotalPrice();
-    menu = new Menu();
-    itemOne = new MenuItem("a", "b", true, 1.3);
-    itemTwo = new MenuItem("c", "d", false, 3.4);
-  }
-
-  @Test
-  public void testItemOnePrice() {
-    command.execute(itemOne);
-    assertEquals("Expects exact price.", 1.3, command.getPrice(), DOUBLE_PRECISION);
-  }
-
-  @Test
-  public void testItemTwoPrice() {
-    command.execute(itemTwo);
-    assertEquals("Expects exact price.", 3.4, command.getPrice(), DOUBLE_PRECISION);
-  }
-
-  @Test
-  public void testMenuPrice() {
-    menu.addItem(itemOne);
-    menu.addItem(itemTwo);
-    command.execute(menu);
-    assertEquals("Expects exact price.", 4.7, command.getPrice(), DOUBLE_PRECISION);
+  @Override
+  protected AbstractSumPrice newCommand() {
+    return new SumTotalPrice();
   }
 }
--- src/test/java/com/cc3002/auxiliar/design/pattern/command/example05/test/SumVegeterianPriceTest.java
+++ src/test/java/com/cc3002/auxiliar/design/pattern/command/example05/test/SumVegeterianPriceTest.java
@@ -1,53 +1,23 @@
 package com.cc3002.auxiliar.design.pattern.command.example05.test;
 
-import static org.junit.Assert.assertEquals;
-
-import com.cc3002.auxiliar.design.pattern.command.example05.IMenu;
-import com.cc3002.auxiliar.design.pattern.command.example05.IMenuItem;
-import com.cc3002.auxiliar.design.pattern.command.example05.Menu;
-import com.cc3002.auxiliar.design.pattern.command.example05.MenuItem;
+import com.cc3002.auxiliar.design.pattern.command.example05.AbstractSumPrice;
 import com.cc3002.auxiliar.design.pattern.command.example05.SumVegeterianPrice;
 
-import org.junit.Before;
-import org.junit.Test;
+public class SumVegeterianPriceTest extends AbstractSumPriceTest {
 
-public class SumVegeterianPriceTest {
-
-  private static final double DOUBLE_PRECISION = 0.000001d;
-  private SumVegeterianPrice command;
-  private IMenu menu;
-  private IMenuItem itemOne;
-  private MenuItem itemTwo;
-
-  /**
-   * Initialize test cases.
-   */
-  @Before
-  public void setUp() {
-    command = new SumVegeterianPrice();
-    menu = new Menu();
-    itemOne = new MenuItem("a", "b", true, 1.3);
-    itemTwo = new MenuItem("c", "d", false, 3.4);
+  @Override
+  protected AbstractSumPrice newCommand() {
+    return new SumVegeterianPrice();
   }
 
-  @Test
-  public void testItemOnePrice() {
-    command.execute(itemOne);
-    assertEquals("Expects exact price.", 1.3, command.getPrice(), DOUBLE_PRECISION);
+  @Override
+  protected double priceItemTwo() {
+    return 0;
   }
-
-  @Test
-  public void testItemTwoPrice() {
-    command.execute(itemTwo);
-    assertEquals("Expects exact price.", 0d, command.getPrice(), DOUBLE_PRECISION);
-  }
-
-  @Test
-  public void testMenuPrice() {
-    menu.addItem(itemOne);
-    menu.addItem(itemTwo);
-    command.execute(menu);
-    assertEquals("Expects exact price.", 1.3, command.getPrice(), DOUBLE_PRECISION);
+  
+  @Override
+  protected double priceMenu() {
+    return 1.3;
   }
 
 }
